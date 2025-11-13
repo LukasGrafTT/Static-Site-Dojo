023ec387a265b216474833eebf7ae87e4d6e89a7
LukasGrafTT <116341983+LukasGrafTT@users.noreply.github.com>
2025-11-13 21:01:50 +0100

    add HtmlBase Plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,9 @@
+import { HtmlBasePlugin } from "@11ty/eleventy";
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+
   eleventyConfig.addPassthroughCopy("src/css");
 };
